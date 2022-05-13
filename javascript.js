9a48c3d0f3817d2cd448158594eaba4aae3406af
Zenzor33 <rzeidler@gmail.com>
2022-05-13 14:02:52 -0300

    fixed spaces

--- javascript.js
+++ javascript.js
@@ -7,23 +7,23 @@ Task 1:
 
 // create a container div to hold the 16x16 square divs
 function createContainer() {
-	const body = document.querySelector('body');
-	const gridContainer = document.createElement('div');
-	gridContainer.classList.add('container');
-	body.appendChild(gridContainer);
+  const body = document.querySelector('body');
+  const gridContainer = document.createElement('div');
+  gridContainer.classList.add('container');
+  body.appendChild(gridContainer);
 }
 
 
 // create 16 divs
 function generateGrid(num) {
-	createContainer();
-	const gridContainer = document.querySelector('.container');
-	for (let i=0; i < num; i++) {
-  	let div = document.createElement('div');
-		div.classList.add('square');
-		div.textContent = `${i+1}`;
-  	gridContainer.appendChild(div);
-  	// console.log(`generated grids: ${i+1}`)
+  createContainer();
+  const gridContainer = document.querySelector('.container');
+  for (let i=0; i < num; i++) {
+    let div = document.createElement('div');
+	  div.classList.add('square');
+	  div.textContent = `${i+1}`;
+    gridContainer.appendChild(div);
+    // console.log(`generated grids: ${i+1}`)
   }
 }
 
