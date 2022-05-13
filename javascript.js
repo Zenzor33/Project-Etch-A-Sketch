2481f0c6e2f8e51b685d55091e4b67842d88c5df
Zenzor33 <rzeidler@gmail.com>
2022-05-13 15:31:15 -0300

    steps 1-3 complete

--- javascript.js
+++ javascript.js
@@ -1,16 +1,29 @@
-/*
-Task 1: 
-- Create a webpage with a 16x16 grid of square divs.
-- Create the divs using JavaScript.
-- It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
-*/
-
 // create a container div to hold the 16x16 square divs
 function createContainer() {
   const body = document.querySelector('body');
   const gridContainer = document.createElement('div');
   gridContainer.classList.add('container');
   body.appendChild(gridContainer);
+}
+
+// Set up an event listener for all divs in the grid container
+function addListener() {
+  const getSquares = document.getElementsByClassName('square');
+	const squares = Array.from(getSquares);
+	console.log(squares);
+	squares.forEach(square => {
+		square.addEventListener('mouseenter', function () {
+			// console.log(`Square number ${square.textContent}`);
+			square.classList = 'squareEnter';
+		})
+	})
+	squares.forEach(square => {
+		square.addEventListener('mouseleave', function () {
+			// console.log(`Square number ${square.textContent}`);
+			square.classList = 'squareExit'
+
+		})
+	})
 }
 
 
@@ -22,6 +35,7 @@ function generateGrid(num) {
     let div = document.createElement('div');
 	  div.classList.add('square');
 	  div.textContent = `${i+1}`;
+		addListener();
     gridContainer.appendChild(div);
     // console.log(`generated grids: ${i+1}`)
   }
@@ -29,4 +43,12 @@ function generateGrid(num) {
 
 generateGrid(16*16);
 
+/*
+Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
+Also check out prompts.
+You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
 
+*/
+
+// add button to top of the screen
